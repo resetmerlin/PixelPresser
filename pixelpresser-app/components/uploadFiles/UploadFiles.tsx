3feff6aa5b2fb459d7d656d459dfd4a3d3b32066
resetmerlin <resetmerlin@gmail.com>
2024-02-10 20:37:38 +0900

    feat: Add acceptance of upload image

--- pixelpresser-app/components/uploadFiles/UploadFiles.tsx
+++ pixelpresser-app/components/uploadFiles/UploadFiles.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { useDropzone } from "react-dropzone";
 import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
@@ -11,35 +11,56 @@ export const UploadFiles = () => {
   const { acceptedFiles, getRootProps, getInputProps, isDragReject } =
     useDropzone({
       accept: {
-        "image/*": [".jpeg", ".png", ".jpg", ".gif"],
+        "image/jpeg": [".jpeg", ".jpg"],
+        "image/png": [".png"],
+        "image/gif": [".gif"],
       },
     });
 
   const files = acceptedFiles.map((file: File) => {
-    console.log("originalFile instanceof Blob", file instanceof Blob); // true
-    console.log(`originalFile size ${file.size / 1024 / 1024} MB`);
+    const fileUrl = URL.createObjectURL(file);
+    const key = `${file.name}-${file.lastModified}`;
 
     return (
-      <li key={file.path}>
+      <div
+        className="rounded-lg w-[10rem] h-[10rem] border-sky-600 text-black border-2 p-1 relative"
+        key={key}
+      >
+        <div className="w-full h-full absolute top-0 left-0  z-10 bg-black/[.3] text-white font-semibold text-sm">
+          <p className="overflow-hidden flex flex-nowrap h-[1.2rem]">
+            {" "}
+            {file.name}
+          </p>
+        </div>
         <Image
-          src={`/${file.path}`}
+          src={fileUrl}
           alt="image__file"
-          width={300}
-          height={300}
+          width={200}
+          height={200}
+          className="absolute z-0 top-0 left-0"
         />
-        {file.size} bytes
-      </li>
+      </div>
     );
   });
 
-  console.log(isDragReject);
-
   return (
     <>
-      <div
-        {...getRootProps({ className: "dropzone" })}
-        className="flex-col flex items-center justify-center w-[65rem] bg-white rounded h-[26rem] relative top-2/4 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]"
-      >
+      <div className="flex-col flex items-center justify-center w-[65rem] bg-white rounded h-[26rem] relative top-2/4 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]">
+        {isDragReject && (
+          <Alert className="w-[32rem] fixed top-5">
+            <Terminal className="h-4 w-4" />
+            <AlertTitle> Wrong file format</AlertTitle>
+            <AlertDescription>Allowed gif, jpeg, jpg, png</AlertDescription>
+          </Alert>
+        )}
+        <div>
+          <Button className="m-3 bg-sky-500">
+            <label htmlFor="uploadFile">Upload Files</label>
+          </Button>
+          <Button className="m-3" variant={"destructive"}>
+            Clear All Files
+          </Button>
+        </div>
         <input
           type="file"
           name="uploadFile"
@@ -47,28 +68,11 @@ export const UploadFiles = () => {
           className="hidden"
           {...getInputProps()}
         />
-        {isDragReject && (
-          <Alert>
-            <Terminal className="h-4 w-4" />
-            <AlertTitle>Heads up!</AlertTitle>
-            <AlertDescription>
-              You can add components to your app using the cli.
-            </AlertDescription>
-          </Alert>
-        )}
-        <div>
-          <Button className="m-3 bg-sky-500">Upload Files</Button>
-          <Button className="m-3" variant={"destructive"}>
-            Clear All Files
-          </Button>
-        </div>
-
-        <div className="flex items-center justify-center border-dashed	border-2 border-black w-4/5		 rounded h-3/4 text-slate-400 ">
+        <div
+          {...getRootProps({ className: "dropzone" })}
+          className="flex items-center justify-center border-dashed	border-2 border-black w-4/5	rounded h-3/4 text-slate-400 "
+        >
           {files ? <p>{files}</p> : <span> Drop Your Files Here</span>}
-
-          <div className="rounded-lg w-[10rem] h-[10rem] border-sky-600 text-black border-2">
-            test
-          </div>
         </div>
 
         <Button className="m-3" variant={"default"}>
